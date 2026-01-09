d61e0cb8af6cdba4ce8c7bc0efb7eb27daf00f61
KarritPrimorph <marcin.sarewicz@gmail.com>
2026-01-09 21:28:00 +0100

    License defined

--- assets/IconToWidget.py
+++ assets/IconToWidget.py
@@ -1,3 +1,17 @@
+# Eleana
+# Copyright (C) 2026 Marcin Sarewicz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+
+
 import customtkinter as ctk
 from PIL import Image
 from pathlib import Path
